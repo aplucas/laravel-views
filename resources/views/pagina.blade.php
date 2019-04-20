31aa0d1a421e2c1da7c16d2f1f35846bdecf18fb
aplucas <l.alexandre100@gmail.com>
2019-04-20 09:43:25 -0300

    registrando o component em app/Providers/AppServiceProvider.php

--- resources/views/pagina.blade.php
+++ resources/views/pagina.blade.php
@@ -7,18 +7,46 @@
 	</head>
 	<body>
 		<div class="container">
+			{{--
 			@component('components.meucomponente', ['titulo'=>'Erro Fatal','tipo'=>'danger'] )
-			{{-- @slot('titulo')
+			@slot('titulo')
 			Erro Fatal!
 			@endslot
 			@slot('tipo')
 			danger
-			@endslot --}}
+			@endslot
 			<strong>Erro: </strong> Mensagem de erro.
 			@endcomponent
 			@component('components.meucomponente', ['titulo'=>'Atenção','tipo'=>'warning'] )
 			<strong>Atenção: </strong> Mensagem de atenção.
 			@endcomponent
+			--}}
+
+			@alerta(['titulo'=>'Atenção','tipo'=>'primary'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'secondary'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'success'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'danger'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'warning'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'info'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'light'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+			@alerta(['titulo'=>'Atenção','tipo'=>'dark'] )
+			<strong>Atenção: </strong> Mensagem de atenção.
+			@endalerta
+
 		</div>
 		{{-- script --}}
 		{{-- <script src="{{ asset('js/app.js') }}" type="text/javascript" ></script> --}}
