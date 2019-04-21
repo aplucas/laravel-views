2141082ff2bcaf55a069d2d8685a9438bc12d208
aplucas <l.alexandre100@gmail.com>
2019-04-21 12:54:33 -0300

    intens do foreach loop->index|iteration|remaining|count

--- resources/views/foreach.blade.php
+++ resources/views/foreach.blade.php
@@ -3,14 +3,38 @@
 @if (isset($produtos))
 	@section('minha_secao_produtos')
 		<p>Loop foreach - Array associativo </p>
-
 		@foreach ($produtos as $p)
-			@alerta(['titulo'=>'Produto','tipo'=>'primary'] )
-				<strong>Id: </strong> {{ $p['id'] }}.
-				<strong>Nome: </strong> {{ $p['nome'] }}.
+<?php
+if ($loop->first) {
+	$titulo = "Produto (primeiro)";
+} elseif ($loop->last) {
+	$titulo = "Produto (último)";
+} else {
+	$titulo = "Produto";
+}
+?>
+			@alerta(['titulo'=>"$titulo",'tipo'=>'primary'] )
+				<strong>Índice: </strong>
+				<span class='badge badge-secondary'>
+					{{ $loop->index }}
+				</span><br>
+				<strong>Interação: </strong>
+				<span class='badge badge-secondary'>
+					{{ $loop->iteration }}
+				</span><br>
+				<strong>Quanto falta: </strong>
+				<span class='badge badge-secondary'>
+					{{ $loop->remaining }}
+				</span><br>
+				<strong>Total: </strong>
+				<span class='badge badge-secondary'>
+					{{ $loop->count }}
+				</span><br>
+				<strong>Id: </strong> {{ $p['id'] }}<br>
+				<strong>Nome: </strong> {{ $p['nome'] }}
 			@endalerta
+
 		@endforeach
-
-
 	@stop
 @endif
+<hr>
